2e37edc34ea7a6a7e4a5ac5bbf0d4d0d7e1b279b
Aibolll <amantaiaibol13@gmail.com>
2025-09-23 12:43:23 +0500

    Initial commit

---
+++ .idea/src/test/java/com/example/aibol/JurieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aibol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JurieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ aibol/src/main/java/com/example/aibol/JurieApplication.java
@@ -0,0 +1,13 @@
+package com.example.aibol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JurieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JurieApplication.class, args);
+	}
+
+}
